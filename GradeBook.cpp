78f54e2f4d8d466bf7bf482892b66186cb096c06
Mqseal <30034267@cc.peralta.edu>
2026-05-04 19:03:44 +0000

    Create stubs and ensure project builds

---
+++ GradeBook.cpp
@@ -0,0 +1,16 @@
+#include "GradeBook.h"
+
+GradeBook::GradeBook() {
+}
+
+void GradeBook::addGrade(const std::string& assignment, double score) {
+}
+
+double GradeBook::getAverage() const {
+}
+
+double GradeBook::getHighest() const {
+}
+
+int GradeBook::numGrades() const {
+}
